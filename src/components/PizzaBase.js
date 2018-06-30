caea418165767442be2cdef2d7e7087385e0577a
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-30 22:31:01 +0200

    tried to work wit redux forms , but cant really get it yet

--- src/components/PizzaBase.js
+++ src/components/PizzaBase.js
@@ -1,36 +1,82 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { Field, reduxForm, formValueSelector } from 'redux-form'
+import { Field, reduxForm, formValueSelector } from "redux-form";
 
 class PizzaBase extends React.PureComponent {
-
-  state = { value: ''}
-handleChange(event){
-  this.setState({value: event.target.value})
-}
 
 
   componentDidMount() {}
 
   render() {
-      console.log(this.props);
-    return (
-      <form>
+    console.log(this.props);
 
-        Choose your pizza base
-        <select>{this.props.pizzaBase.map(base =>{
-        return (<option value="{base.size}" className="baseSize">Basesize of {base.size} cm</option>)
-        })
-      }</select>
-      <submit>Submit</submit>
-    </form>
+ const { handleSubmit, pristine, reset, submitting } = this.props
+
+ const validate = values => {
+   const errors = {}
+   if (!values.base.id) {
+     errors.pizzaBase = console.log('Please fill in the size you want')
+   } else if (values.base.id.length > 2) {
+     errors.username = console.log('You can not choose more than two')
+   }
+
+   return errors
+ }
+ const renderField = props => (
+   <div>
+     <label>{props.placeholder}</label>
+     <div>
+       <input {...props}/>
+       {props.touched && props.error && <span>{props.error}</span>}
+     </div>
+   </div>
+ )
+    return (
+      <div>
+        <form>
+
+
+          Choose your pizza base
+
+          {this.props.pizzaBase.map(base => {
+            return (
+              <div className="baseSizeBTNContainer">
+                <label htmlFor="baseSizeLabel">I would like a Pizzabase of {base.size} cm</label>
+                <div>
+                  <Field
+                    name={base.id}
+                    id="baseSize"
+                    component="input"
+                    type="checkbox"
+                    className="baseSizecheckbox"
+                  />
+                </div> <br />
+              </div>
+            );
+          })}
+
+               <div>
+                 <button type="submit" disabled={pristine || submitting}>Submit</button>
+                 <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+               </div>
+        </form>
+      </div>
     );
   }
 }
-const mapStateToProps = function (state) {
+const mapStateToProps = function(state) {
   return {
-    pizzaBase: state.pizzaBase
-  }
-}
+    pizzaBase: state.pizzaBase,
+    // form: state.form
 
-export default connect(mapStateToProps,{})(PizzaBase);
+  };
+};
+PizzaBase = reduxForm({
+  form: "selectingFormValues" // a unique identifier for this form
+})(PizzaBase);
+
+export default connect(mapStateToProps, {
+  Field,
+  reduxForm,
+  formValueSelector
+})(PizzaBase);
--- src/components/PizzaSauce.js
+++ src/components/PizzaSauce.js
@@ -1,19 +1,52 @@
-import * as React from "react";
+
 import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-
-class PizzaSauce extends React.PureComponent {
-  componentDidMount() {}
-
+class PizzaSauce extends Component {
   render() {
+    const { handleSubmit } = this.props;
     return (
       <div>
-        Sauces
-      </div>
+      {/* <form onSubmit={handleSubmit}>
+        <div>
+          <label>First Name</label>
+          <Field name="firstName" component="input" type="text"/>
+        </div>
+        <div>
+          <label>Last Name</label>
+          <Field name="lastName" component="input" type="text"/>
+        </div>
+        <div>
+          <label>Email</label>
+          <Field name="email" component="input" type="email"/>
+        </div>
+        <button type="submit">Submit</button>
+      </form> */}
+    </div>
     );
   }
 }
-const mapStateToProps = state => {
-  return { state };
-};
-export default connect(mapStateToProps,{})(PizzaSauce);
+
+// Decorate the form component
+PizzaSauce = reduxForm({
+  form: 'contact' // a unique name for this form
+})(PizzaSauce);
+
+export default PizzaSauce;
+
+// class PizzaSauce extends React.PureComponent {
+//   componentDidMount() {}
+//
+//   render() {
+//     return (
+//       <div>
+//         Sauces
+//       </div>
+//     );
+//   }
+// }
+// const mapStateToProps = state => {
+//   return { state };
+// };
+// export default connect(mapStateToProps,{})(PizzaSauce);
